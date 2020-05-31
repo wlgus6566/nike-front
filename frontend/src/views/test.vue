5d3f478f0888dd5ce40230af71b1a6b083af4b3b
염주성 <furia0928@gmail.com>
2020-05-31 15:12:04 +0900

    package json 수정

---
+++ frontend/src/views/test.vue
@@ -0,0 +1,37 @@
+<template>
+	<div id="transition-components-demo" class="demo">
+		<input type="radio" value="v-a" id="a" name="view">
+		<label for="a">A</label>
+		<input type="radio" value="v-b" id="b" name="view">
+		<label for="b">B</label>
+		<transition name="component-fade" mode="out-in">
+			<component v-bind:is="view"></component>
+		</transition>
+	</div>
+</template>
+<script>
+	export default {
+		data() {
+			return {
+				view: 'v-a'
+			}
+		},
+		components: {
+			'v-a': {
+				template: '<div>Component A</div>'
+			},
+			'v-b': {
+				template: '<div>Component B</div>'
+			}
+		}
+	}
+</script>
+<style scoped>
+	.component-fade-enter-active, .component-fade-leave-active {
+		transition:opacity .3s ease;
+	}
+	.component-fade-enter, .component-fade-leave-to
+		/* .component-fade-leave-active below version 2.1.8 */ {
+		opacity:0;
+	}
+</style>
