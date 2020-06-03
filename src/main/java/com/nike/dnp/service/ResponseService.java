90de3522452580f0961e4571d6de5aa0aa5b9159
sojeong.lee <sojeong.lee@emotion.co.kr>
2020-06-03 11:31:05 +0900

    [공통] [WIP] 에러 처리

--- src/main/java/com/nike/dnp/service/ResponseService.java
+++ src/main/java/com/nike/dnp/service/ResponseService.java
@@ -31,6 +31,7 @@ public class ResponseService {
             return msg;
         }
     }
+
     // 단일건 결과를 처리하는 메소드
     public <T> SingleResult<T> getSingleResult(T data) {
         SingleResult<T> result = new SingleResult<>();
@@ -38,6 +39,7 @@ public class ResponseService {
         setSuccessResult(result);
         return result;
     }
+
     public <T> SingleResult<T> getSingleResult(T data, String msg) {
         SingleResult<T> result = new SingleResult<>();
         result.setData(data);
@@ -47,6 +49,7 @@ public class ResponseService {
         setSuccessResult(result);
         return result;
     }
+
     // 다중건 결과를 처리하는 메소드
     public <T> ListResult<T> getListResult(List<T> list) {
         ListResult<T> result = new ListResult<>();
@@ -54,13 +57,15 @@ public class ResponseService {
         setSuccessResult(result);
         return result;
     }
+
     // 성공 결과만 처리하는 메소드
     public CommonResult getSuccessResult() {
         CommonResult result = new CommonResult();
         setSuccessResult(result);
         return result;
     }
-    // 실패 결과만 처리하는 메소드
+
+    // 실패 결과 처리하는 메소드
     public CommonResult getFailResult() {
         CommonResult result = new CommonResult();
         result.setSuccess(false);
@@ -68,6 +73,7 @@ public class ResponseService {
         result.setMsg(CommonResponse.FAIL.getMsg());
         return result;
     }
+
     // 결과 모델에 api 요청 성공 데이터를 세팅해주는 메소드
     private void setSuccessResult(CommonResult result) {
         result.setSuccess(true);
