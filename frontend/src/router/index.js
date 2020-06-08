c3dec4f6276e5711267b60c4c22c1350f51c36c5
furia0928 <furia0928@gmail.com>
2020-06-08 21:04:31 +0900

    layout update

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,28 +1,30 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import test from '@/views/test'
-import main from '@/views/main.vue'
-import asset from '@/views/asset'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const router = new VueRouter({
     mode: 'history',
     routes: [
         {
             path: '/test',
-            component: test,
+            component: () => import('@/views/test'),
             meta: { layout: 'default' },
         },
         {
             path: '/',
-            component: main,
-            meta: { layout: 'clean' },
+            component: () => import('@/views/main.vue'),
+            meta: { layout: 'default' },
         },
         {
             path: '/asset',
-            component: asset,
+            component: () => import('@/views/asset'),
             meta: { layout: 'default', aside: 'order' },
+        },
+        {
+            path: '/login',
+            component: () => import('@/views/login'),
+            meta: { layout: 'clean', aside: 'order' },
         },
         {
             path: '*',
@@ -30,28 +32,6 @@ const router = new VueRouter({
             meta: { layout: 'clean' },
         },
     ],
-})
+});
 
-/*router.afterEach((to, from) => {
-
-});*/
-
-// router.beforeEach((to, from, next) => {
-// 	if (to.matched.some(record => record.meta.requiresAuth)) {
-// 		// 이 라우트는 인증이 필요하며 로그인 한 경우 확인하십시오.
-// 		// 그렇지 않은 경우 로그인 페이지로 리디렉션하십시오.
-// 		if (!auth.loggedIn()) {
-// 			next({
-// 				path: '/login',
-// 				query: {redirect: to.fullPath}
-// 			})
-// 		} else {
-// 			next()
-// 		}
-// 	} else {
-// 		next() // 반드시 next()를 호출하십시오!
-// 	}
-// 	next()
-// })
-
-export default router
+export default router;
--- frontend/src/views/layouts/clean.vue
+++ frontend/src/views/layouts/clean.vue
@@ -1,11 +1,17 @@
 <template>
-    <div>
-        123
+    <div id="wrap">
+        <section id="container">
+            <div class="contents">
+                <transition name="contents-ani" mode="out-in">
+                    <router-view></router-view>
+                </transition>
+            </div>
+        </section>
     </div>
 </template>
 <script>
 export default {
     name: 'cleanLayout',
-}
+};
 </script>
 <style scoped></style>
