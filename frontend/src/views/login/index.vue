4477ffd9c3b4e7ffed6ce3f8454dad3817cfa1da
Seulgi Han <seulgi.han@emotion.co.kr>
2020-06-10 14:17:39 +0900

    로그인 > 입력/이메일 인증 페이지 작업중

--- frontend/src/views/login/index.vue
+++ frontend/src/views/login/index.vue
@@ -7,12 +7,12 @@
                 </a>
             </h1>
             <div class="login-form">
-                <form action="" method="">
+                <form action="" method="" v-if="certification === 'success'">
                     <fieldset>
                         <legend>NIKE ND&P SERVICE</legend>
                         <h2 class="title">
-                            NIKE KOREA<span>는</span><br />PARTNER<span>
-                                와 함께합니다.</span
+                            NIKE KOREA<span>는<br /> </span>PARTNER<span
+                                >와 함께합니다.</span
                             >
                         </h2>
                         <div class="input-group">
@@ -31,12 +31,97 @@
                                 placeholder="PW"
                             />
                         </div>
-                        <button type="button" class="btn-login">
+                        <button
+                            type="button"
+                            class="btn-login"
+                            @click="changeLogin"
+                        >
                             LOGIN
                         </button>
                         <a href="javascript:void(0)" class="pw-find"
                             >비밀번호를 찾으시겠습니까?</a
                         >
+                    </fieldset>
+                </form>
+                <form class="email-certification" action="" method="" v-else>
+                    <fieldset>
+                        <legend>NIKE ND&P SERVICE</legend>
+                        <h2 class="title">
+                            이메일 인증
+                        </h2>
+                        <p>
+                            인증메일이 발송되었습니다.<br />메일함을 확인하여
+                            인증코드를 입력해주세요.
+                        </p>
+                        <div class="input-group">
+                            <label for="word1"></label
+                            ><input
+                                type="text"
+                                maxlength="1"
+                                id="word1"
+                                value=""
+                                placeholder=""
+                            />
+                            <label for="word2"></label
+                            ><input
+                                type="text"
+                                maxlength="1"
+                                id="word2"
+                                value=""
+                                placeholder=""
+                            />
+                            <label for="word3"></label
+                            ><input
+                                type="text"
+                                maxlength="1"
+                                id="word3"
+                                value=""
+                                placeholder=""
+                            />
+                            <label for="word4"></label
+                            ><input
+                                type="text"
+                                maxlength="1"
+                                id="word4"
+                                value=""
+                                placeholder=""
+                            />
+                            <label for="word5"></label
+                            ><input
+                                type="text"
+                                maxlength="1"
+                                id="word5"
+                                value=""
+                                placeholder=""
+                            />
+                            <label for="word6"></label
+                            ><input
+                                type="text"
+                                maxlength="1"
+                                id="word6"
+                                value=""
+                                placeholder=""
+                            />
+                            <label for="word7"></label
+                            ><input
+                                type="text"
+                                maxlength="1"
+                                id="word7"
+                                value=""
+                                placeholder=""
+                            />
+                            <label for="word8"></label
+                            ><input
+                                type="text"
+                                maxlength="1"
+                                id="word8"
+                                value=""
+                                placeholder=""
+                            />
+                        </div>
+                        <button type="button" class="btn-login">
+                            인증하기
+                        </button>
                     </fieldset>
                 </form>
             </div>
@@ -49,8 +134,17 @@ export default {
     name: 'login',
     data: function () {
         return {
-            //loginType: 'nd',
+            certification: 'success',
         };
+    },
+    methods: {
+        changeLogin() {
+            if (this.certification === 'success') {
+                this.certification = '';
+            } else {
+                this.certification = 'success';
+            }
+        },
     },
 };
 </script>
@@ -74,16 +168,19 @@ export default {
     position: relative;
     top: -230px;
 }
+
 .logo a {
     display: block;
     text-align: center;
 }
+
 section {
     position: absolute;
     left: 50%;
     top: 50%;
     transform: translate(-50%, -50%);
 }
+
 section .text {
     margin-top: 23px;
     font-size: 14px;
@@ -91,6 +188,7 @@ section .text {
     color: #888;
     letter-spacing: -0.5px;
 }
+
 section .text strong {
     color: #000;
 }
@@ -101,51 +199,102 @@ section .text strong {
     top: 50%;
     transform: translate(-50%, -50%);
     width: 370px;
+    height: 390px;
     margin: 160px auto 0;
-    padding: 50px 45px 45px;
+    padding: 50px 45px 75px;
     box-sizing: border-box;
     background-color: rgba(0, 0, 0, 0.7);
     box-shadow: -20px 20px 8px 0 rgba(0, 0, 0, 0.5);
     text-align: left;
 }
+
+.login-form form {
+    position: relative;
+}
+
 .login-form .title {
-    margin-bottom: 30px;
+    margin-bottom: 26px;
     font-weight: 400;
     font-size: 40px;
     font-family: 'Bebas Neue';
-    line-height: 1;
-    letter-spacing: 0.56px;
+    line-height: 1.1;
+    letter-spacing: -0.5px;
     color: #fff;
 }
+
+.login-form .email-certification .title {
+    margin-bottom: 0;
+    font-weight: 700;
+    font-size: 30px;
+    font-family: 'Noto Sans KR', sans-serif;
+    line-height: normal;
+    letter-spacing: -2px;
+}
+
+.login-form .email-certification .title + p {
+    margin: 9px 0 40px;
+    font-weight: 400;
+    font-size: 14px;
+    letter-spacing: -0.5px;
+    opacity: 0.5;
+    color: #fff;
+}
+
 .login-form .title span {
     font-weight: 200;
     font-size: 24px;
     letter-spacing: -0.5px;
 }
-.login-form .title + .radio-group {
-    margin-top: -10px;
-}
 
 .input-group {
     width: 100%;
 }
+
 .input-group input {
     display: block;
     width: 100%;
+    height: 40px;
     padding: 10px 15px;
     border: 0;
     font-size: 15px;
     background-color: rgba(255, 255, 255, 0.1);
     color: #fff;
 }
+
 .input-group input ~ input {
-    margin-top: 7px;
+    margin-top: 8px;
 }
+
+.email-certification .input-group {
+    display: flex;
+    justify-content: center;
+    margin-bottom: 24px;
+}
+
+.email-certification .input-group input {
+    display: block;
+    width: 20px;
+    height: 40px;
+    padding: 0;
+    border-bottom: 1px solid rgba(255, 255, 255, 0.3);
+    font-size: 24px;
+    font-family: 'Roboto', sans-serif;
+    background-color: transparent;
+    color: #fff;
+    text-align: center;
+}
+
+.email-certification .input-group input ~ input {
+    margin-left: 10px;
+    margin-top: 0;
+}
+
 .input-group input::-webkit-input-placeholder {
     color: #fff;
     opacity: 0.5;
     font-family: 'Bebas Neue';
 }
+
 .input-group input:-ms-input-placeholder {
     color: #888;
     font-family: 'Bebas Neue';
@@ -154,21 +303,32 @@ section .text strong {
 .btn-login {
     width: 100%;
     height: 45px;
-    line-height: 45px;
     margin-top: 20px;
     border-radius: 0;
-    background-color: #fa5400;
     font-size: 20px;
     font-family: 'Bebas Neue';
-    color: #fff;
+    background-color: #fa5400;
+    line-height: 45px;
     letter-spacing: 0.5px;
+    color: #fff;
 }
+
+.email-certification .btn-login {
+    font-size: 16px;
+    font-family: inherit;
+    line-height: normal;
+    letter-spacing: -0.57px;
+}
+
 .pw-find {
-    display: inline-block;
+    display: block;
+    position: absolute;
+    left: 0;
+    bottom: -30px;
     margin-top: 12px;
-    font-size: 12px;
-    border-bottom: 1px solid rgba(255, 255, 255, 0.5);
     padding-bottom: 1px;
+    border-bottom: 1px solid rgba(255, 255, 255, 0.5);
+    font-size: 12px;
     color: rgba(255, 255, 255, 0.5);
 }
 </style>
