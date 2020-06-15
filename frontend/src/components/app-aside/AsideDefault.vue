4341cff177383842d763d6c89dc13d1fe327650e
furia0928 <furia0928@gmail.com>
2020-06-15 21:44:14 +0900

    [publishing] aside animation 작업중

--- frontend/src/components/app-aside/AsideDefault.vue
+++ frontend/src/components/app-aside/AsideDefault.vue
@@ -1,9 +1,11 @@
 <template>
     <aside class="sticky-container" sticky-container>
-        <div class="inner" on-stick="onStick" sticky-offset="{top: 10, bottom: 30}" sticky-side="both" sticky-z-index="20" v-sticky>
-            <div class="aside-wrap">
-                <UserInfo></UserInfo>
-                <TabComponent v-bind:tabMenus="tabMenus"></TabComponent>
+        <div class="inner" sticky-offset="{top:0, bottom:0}" sticky-side="both" sticky-z-index="20" v-sticky="shouldStick">
+            <div class="test">
+                <div class="aside-wrap">
+                    <UserInfo></UserInfo>
+                    <TabComponent v-bind:tabMenus="tabMenus"></TabComponent>
+                </div>
             </div>
         </div>
     </aside>
@@ -11,9 +13,12 @@
 <script>
 import Sticky from 'vue-sticky-directive';
 import UserInfo from './UserInfo.vue';
-import TabComponent from '@/components/tab-components/index.vue';
+import TabComponent from '@/components/tab-components/';
 
 export default {
+    name: 'AsideDefault',
+    props: ['shouldStick'],
+    watch: {},
     data: function () {
         return {
             tabMenus: {
@@ -25,18 +30,8 @@ export default {
                     },
                 ],
             },
-            tabHisroty: {
-                tabClass: 'tab-list',
-                tabList: [
-                    {
-                        title: 'MYPAGE',
-                        component: 'ContentMypage',
-                    },
-                ],
-            },
         };
     },
-    name: 'AsideDefault',
     directives: {
         Sticky,
     },
@@ -44,7 +39,6 @@ export default {
         UserInfo,
         TabComponent,
     },
-    methods: {},
 };
 </script>
 <style scoped></style>
