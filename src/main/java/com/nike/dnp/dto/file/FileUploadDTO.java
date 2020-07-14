b710b9f7968825e6ebfb7322a6fb681325402cb4
taehoYun <yuntaeho@emotion.co.kr>
2020-07-14 18:13:19 +0900

    2020/07/14 18:13 시큐리티 적용

--- src/main/java/com/nike/dnp/dto/file/FileUploadDTO.java
+++ src/main/java/com/nike/dnp/dto/file/FileUploadDTO.java
@@ -5,10 +5,15 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 @Getter
 @Setter
 public class FileUploadDTO {
 
 	@ApiParam(hidden = true)
 	private MultipartFile uploadFile;
+
+	@ApiParam(hidden = true)
+	private List<MultipartFile> uploadFileList;
 }
