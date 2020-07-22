0e11aba03ffe32bfd3bdef8a4dee17b44f424015
Seulgi Han <seulgi.han@emotion.co.kr>
2020-07-22 10:57:49 +0900

    newslist

--- frontend/src/components/news-list/index.vue
+++ frontend/src/components/news-list/index.vue
@@ -1,30 +1,13 @@
 <template>
     <ul class="news-list">
-        <li>
+        <li class="news-list-item" v-for="(item, index) in items" :key="index">
             <a href="#">
                 <span class="thumbnail">
-                    <img src="@/assets/images/svg/img-asset-none.svg" alt="" />
+                    <img :src="item.img" alt="" />
                 </span>
                 <span class="info-box">
-                    <strong class="title"
-                        >글로벌 스포츠 브랜드 나이키, 브랜드 문화 공간 마이클 조던을 담은 ‘조던
-                        서울’ 오픈 포토리캡11111</strong
-                    >
-                    <span class="esc">2020.06.17</span>
-                </span>
-            </a>
-        </li>
-        <li>
-            <a href="#">
-                <span class="thumbnail">
-                    <img src="@/assets/images/svg/img-asset-none.svg" alt="" />
-                </span>
-                <span class="info-box">
-                    <strong class="title"
-                        >글로벌 스포츠 브랜드 나이키, 브랜드 문화 공간 마이클 조던을 담은 ‘조던
-                        서울’ 오픈 포토리캡</strong
-                    >
-                    <span class="esc">2020.06.17</span>
+                    <strong class="title">{{ item.title }}</strong>
+                    <span class="esc">{{ item.esc }}</span>
                 </span>
             </a>
         </li>
@@ -34,7 +17,24 @@
 export default {
     name: 'newsList',
     data() {
-        return {};
+        return {
+            items: [
+                {
+                    title:
+                        '글로벌 스포츠 브랜드 나이키, 브랜드 문화 공간 마이클 조던을 담은 ‘조던서울’ 오픈 포토리캡11111',
+                    img: require('@/assets/images/svg/img-asset-none.svg'),
+                    url: null,
+                    esc: '2020.06.17',
+                },
+                {
+                    title:
+                        '글로벌 스포츠 브랜드 나이키, 브랜드 문화 공간 마이클 조던을 담은 ‘조던서울’ 오픈 포토리캡2222',
+                    img: require('@/assets/images/svg/img-asset-none.svg'),
+                    url: null,
+                    esc: '2020.06.18',
+                },
+            ],
+        };
     },
     methods: {},
 };
