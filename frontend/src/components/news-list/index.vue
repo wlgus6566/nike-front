4d4f064769f873ad6f55785c2d49b5a640c29b3e
Seulgi Han <seulgi.han@emotion.co.kr>
2020-07-22 10:26:53 +0900

    newslist

---
+++ frontend/src/components/news-list/index.vue
@@ -0,0 +1,42 @@
+<template>
+    <ul class="news-list">
+        <li>
+            <a href="#">
+                <span class="thumbnail">
+                    <img src="@/assets/images/svg/img-asset-none.svg" alt="" />
+                </span>
+                <span class="info-box">
+                    <strong class="title"
+                        >글로벌 스포츠 브랜드 나이키, 브랜드 문화 공간 마이클 조던을 담은 ‘조던
+                        서울’ 오픈 포토리캡11111</strong
+                    >
+                    <span class="esc">2020.06.17</span>
+                </span>
+            </a>
+        </li>
+        <li>
+            <a href="#">
+                <span class="thumbnail">
+                    <img src="@/assets/images/svg/img-asset-none.svg" alt="" />
+                </span>
+                <span class="info-box">
+                    <strong class="title"
+                        >글로벌 스포츠 브랜드 나이키, 브랜드 문화 공간 마이클 조던을 담은 ‘조던
+                        서울’ 오픈 포토리캡</strong
+                    >
+                    <span class="esc">2020.06.17</span>
+                </span>
+            </a>
+        </li>
+    </ul>
+</template>
+<script>
+export default {
+    name: 'newsList',
+    data() {
+        return {};
+    },
+    methods: {},
+};
+</script>
+<style scoped></style>
