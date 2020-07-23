02267e03415ca16800240666b8c0a34c93f60c75
Seulgi Han <seulgi.han@emotion.co.kr>
2020-07-23 20:16:16 +0900

    뉴스/공지사항 관련

--- frontend/src/components/news/notice-list.vue
+++ frontend/src/components/news/notice-list.vue
@@ -14,21 +14,17 @@
                         <th>등록일</th>
                     </tr>
                 </thead>
-                <tbody v-for="(item, index) in items" :key="index">
-                    <tr class="noti" v-if="">
-                        <td>
+                <tbody>
+                    <tr
+                        v-for="(item, index) in items.slice().reverse()"
+                        :key="index"
+                        v-bind:class="[{ noti: item.noti }]"
+                    >
+                        <td v-if="item.noti">
                             <span class="label-noti">중요</span>
                         </td>
-                        <td class="agl">
-                            <a href="#">{{ item.agl }}</a>
-                        </td>
-                        <td>
-                            <span class="date">{{ item.date }}</span>
-                        </td>
-                    </tr>
-                    <tr v-else>
-                        <td>
-                            <span class="num">{{ index }}</span>
+                        <td v-else>
+                            <span class="num">{{ item.num }}</span>
                         </td>
                         <td class="agl">
                             <a href="#">{{ item.agl }}</a>
@@ -49,19 +45,51 @@ export default {
         return {
             items: [
                 {
+                    num: '1',
+                    noti: false,
                     date: '2020.06.18',
-                    agl: 'NIKE 2020 PSKO 일정이 업데이트 되었습니다111.',
+                    agl: '1 NIKE 2020 PSKO 일정이 업데이트 되었습니다.',
                 },
                 {
+                    num: '2',
+                    noti: false,
                     date: '2020.06.18',
-                    agl: 'NIKE 2020 PSKO 일정이 업데이트 되었습니다222.',
+                    agl: '2 NIKE 2020 PSKO 일정이 업데이트 되었습니다.',
                 },
                 {
+                    num: '3',
+                    noti: false,
                     date: '2020.06.18',
-                    agl: 'NIKE 2020 PSKO 일정이 업데이트 되었습니다333.',
+                    agl: '3 NIKE 2020 PSKO 일정이 업데이트 되었습니다.',
+                },
+                {
+                    num: null,
+                    noti: true,
+                    date: '2020.06.18',
+                    agl: '4 NIKE 2020 PSKO 일정이 업데이트 되었습니다.',
+                },
+                {
+                    num: null,
+                    noti: true,
+                    date: '2020.06.18',
+                    agl: '5 NIKE 2020 PSKO 일정이 업데이트 되었습니다.',
+                },
+                {
+                    num: null,
+                    noti: true,
+                    date: '2020.06.18',
+                    agl: '6 NIKE 2020 PSKO 일정이 업데이트 되었습니다',
                 },
             ],
         };
+    },
+    filters: {
+        reverse: function (array) {
+            return array.slice().reverse();
+        },
+    },
+    mounted() {
+        console.log(this.items);
     },
     computed: {},
     methods: {},
