01b444be90170e6a65e0ffb5c1921bf256b81aa6
furia0928 <furia0928@gmail.com>
2020-08-02 18:29:23 +0900

    error 수정

--- frontend/src/views/pages/management/group.vue
+++ frontend/src/views/pages/management/group.vue
@@ -1,28 +1,37 @@
 <template>
     <div>
         <h2 class="page-title">권한 그룹관리</h2>
+
         <div class="group-management">
-            <div class="group-tree">
-                <div class="group-tree-fixed">
-                    <div class="group-tree-inner">
-                        <h3>전체 권한 그룹</h3>
-                        <ul>
-                            <li>
-                                <span>NSP KEY ACCOUNT</span>
-                                <button>toggle</button>
-                                <ul>
-                                    <li>
-                                        <span>대연</span>
-                                        <button>toggle</button>
-                                    </li>
-                                </ul>
-                            </li>
-                            <li>
-                                <span>NSP KEY ACCOUNT</span>
-                                <button>toggle</button>
-                            </li>
-                        </ul>
-                    </div>
+            <div class="group-tree sticky-container" sticky-container>
+                <div
+                    class="group-tree-fixed sticky-content"
+                    sticky-offset="{top:0, bottom:0}"
+                    sticky-side="both"
+                    sticky-z-index="20"
+                    v-sticky
+                >
+                    <el-scrollbar view-class="view-box" :native="false">
+                        <div class="group-tree-inner">
+                            <h3>전체 권한 그룹</h3>
+                            <ul>
+                                <li>
+                                    <span>NSP KEY ACCOUNT</span>
+                                    <button>toggle</button>
+                                    <ul>
+                                        <li>
+                                            <span>대연</span>
+                                            <button>toggle</button>
+                                        </li>
+                                    </ul>
+                                </li>
+                                <li>
+                                    <span>NSP KEY ACCOUNT</span>
+                                    <button>toggle</button>
+                                </li>
+                            </ul>
+                        </div>
+                    </el-scrollbar>
                     <div>
                         <button>삭제</button>
                         <button>추가</button>
@@ -34,8 +43,12 @@
     </div>
 </template>
 <script>
+import Sticky from 'vue-sticky-directive';
 export default {
     name: 'group',
+    directives: {
+        Sticky,
+    },
 };
 </script>
 <style scoped>
@@ -45,8 +58,12 @@ export default {
 .group-tree {
     flex: 0 0 220px;
     width: 220px;
+    background: red;
 }
 .group-tree-fixed {
+    background: blue;
+    width: 220px;
+    box-sizing: border-box;
     border: 1px solid #ddd;
 }
 .group-detail {
