ebd2de1cc5e5572e4500720c69ea371ef6a71c21
taehoYun <yuntaeho@emotion.co.kr>
2020-08-07 18:13:46 +0900

    2020/08/07 16:12 pmd 수정

--- src/main/java/com/nike/dnp/dto/calendar/CalendarCheckDTO.java
+++ src/main/java/com/nike/dnp/dto/calendar/CalendarCheckDTO.java
@@ -4,6 +4,12 @@ import lombok.*;
 
 import java.time.LocalDateTime;
 
+/**
+ * The Class Calendar check dto.
+ *
+ * @author [윤태호]
+ * @since 2020. 8. 7. 오후 6:13:32
+ */
 @Getter
 @Setter
 @ToString
@@ -12,8 +18,18 @@ import java.time.LocalDateTime;
 public class CalendarCheckDTO {
 
 
+	/**
+	 * The Begin dt
+	 *
+	 * @author [윤태호]
+	 */
 	private LocalDateTime BeginDt;
 
+	/**
+	 * The Count
+	 *
+	 * @author [윤태호]
+	 */
 	private Long count;
 
 }
