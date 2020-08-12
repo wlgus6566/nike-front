9d18db48ff6d591729ae7ba3ae7779f849c542c9
jungmin.lee <jungmin.lee@emotion.co.kr>
2020-08-12 15:42:15 +0900

    FAQ 등록,수정

--- frontend/src/views/pages/mypage/faq-form.vue
+++ frontend/src/views/pages/mypage/faq-form.vue
@@ -1,9 +1,220 @@
-<template> faq 등록</template>
+<template>
+    <div>
+        <h2 class="page-title">
+            <span class="ko">{{ this.$route.meta.title }}</span>
+        </h2>
+        <form @submit.prevent="submitData">
+            <h3 class="form-title mt20">등록/수정</h3>
+            <hr class="hr-black" />
+            <ul class="form-list">
+                <li class="form-row">
+                    <div class="form-column">
+                        <span class="label-title required">카테고리</span>
+                    </div>
+                    <div class="form-column">
+                        <span class="select">
+                            <el-select
+                                v-model="categoryCodeList.value"
+                                required
+                            >
+                                <el-option
+                                    v-for="(item,
+                                    index) in categoryCodeList.listSortOptions"
+                                    :key="index"
+                                    :label="item.label"
+                                    :value="item.value"
+                                />
+                            </el-select>
+                        </span>
+                    </div>
+                </li>
+                <li class="form-row">
+                    <div class="form-column">
+                        <label class="label-title required">질문</label>
+                    </div>
+                    <div class="form-column">
+                        <span class="textarea">
+                            <textarea
+                                cols="100"
+                                rows="2"
+                                style="height: 60px;"
+                                v-model="faqDetail.title"
+                                required
+                            ></textarea>
+                        </span>
+                    </div>
+                </li>
+                <li class="form-row">
+                    <div class="form-column">
+                        <label class="label-title required">답변</label>
+                    </div>
+                    <div class="form-column">
+                        <span class="textarea">
+                            <textarea
+                                cols="100"
+                                rows="2"
+                                style="height: 300px;"
+                                v-model="faqDetail.contents"
+                                required
+                            ></textarea>
+                        </span>
+                    </div>
+                </li>
+            </ul>
+            <hr class="hr-gray" />
+            <div class="btn-area">
+                <button type="button" class="btn-s-white" @click="cancelBack()">
+                    <span>취소</span>
+                </button>
+                <button type="submit" class="btn-s-black">
+                    <span>저장</span>
+                </button>
+            </div>
+        </form>
+    </div>
+</template>
 <script>
+import { getCustomerDetail, postFaq, putFaq } from '@/api/customer';
+import { getCode } from '@/api/code';
+
 export default {
     name: 'faq-form',
     data() {
-        return {};
+        return {
+            // title: '',
+            // contents: '',
+            // noticeArticleCategoryCode: null,
+            noticeArticleSeq: null,
+            useYn: this.useYn,
+            categoryCodeList: {
+                listSortOptions: [],
+                value: '',
+            },
+            faqDetail: {
+                title: '',
+                contents: '',
+                noticeArticleCategoryCode: null,
+            },
+        };
+    },
+    mounted() {
+        console.log(this.$route.params.id);
+        this.getCategoryCode();
+        if (this.$route.meta.modify) {
+            this.getFaqDetail();
+        }
+    },
+    methods: {
+        submitData() {
+            if (this.$route.meta.modify) {
+                this.modifyData();
+            } else {
+                this.saveData();
+            }
+        },
+        // FAQ 저장
+        async saveData() {
+            if (!confirm('저장하시겠습니까?')) {
+                return false;
+            }
+            try {
+                const response = await postFaq({
+                    contents: this.faqDetail.contents,
+                    noticeArticleCategoryCode: this.categoryCodeList.value,
+                    noticeArticleSectionCode: 'QNA',
+                    title: this.faqDetail.title,
+                    useYn: this.useYn,
+                });
+                console.log(response);
+                this.$store.commit('SET_RELOAD', true);
+                if (response.data.success) {
+                    console.log('저장');
+                    this.detailDataReset();
+                    this.$router.push('/mypage/faq');
+                }
+            } catch (error) {
+                console.log(error);
+            }
+        },
+
+        //FAQ 수정
+        async modifyData() {
+            console.log(this.categoryCodeList.value);
+            if (!confirm('수정하시겠습니까?')) {
+                return false;
+            }
+            if (this.$route.meta.modify) {
+                try {
+                    const response = await putFaq(this.$route.params.id, {
+                        title: this.faqDetail.title,
+                        contents: this.faqDetail.contents,
+                        noticeArticleCategoryCode: this.categoryCodeList.value,
+                        noticeArticleSectionCode: 'QNA',
+                        noticeArticleSeq: this.$route.params.id,
+                        useYn: this.useYn,
+                    });
+                    console.log(response.data.success);
+                    this.$store.commit('SET_RELOAD', true);
+                    if (response.data.success) {
+                        console.log('수정');
+                        this.detailDataReset();
+                        this.$router.push('/mypage/faq');
+                    }
+                } catch (error) {
+                    console.log(error);
+                }
+            }
+        },
+
+        // 카테고리 코드 목록 조회
+        async getCategoryCode() {
+            try {
+                const {
+                    data: { data: response },
+                } = await getCode('NOTICE_CATEGORY_CODE');
+                //console.log(response);
+
+                response.forEach((el) => {
+                    this.categoryCodeList.listSortOptions.push({
+                        value: el.code,
+                        label: el.codeName,
+                    });
+                });
+            } catch (error) {
+                console.log(error);
+            }
+        },
+
+        //FAQ 상세
+        async getFaqDetail() {
+            console.log(this.$route.params.id);
+            try {
+                const {
+                    data: { data: response },
+                } = await getCustomerDetail(this.$route.params.id);
+                this.faqDetail = response;
+                this.categoryCodeList.value =
+                    response.noticeArticleCategoryCode;
+                console.log(response);
+            } catch (error) {
+                console.log(error);
+            }
+        },
+
+        //작성 취소
+        cancelBack() {
+            if (!confirm('작성을 취소하시겠습니까?')) {
+                return false;
+            }
+            //this.detailDataReset();
+            this.$router.go(-1);
+        },
+        detailDataReset() {
+            this.title = '';
+            this.contents = '';
+            this.noticeArticleCategoryCode = '';
+            this.noticeArticleSeq = '';
+        },
     },
 };
 </script>
--- frontend/src/views/pages/mypage/faq.vue
+++ frontend/src/views/pages/mypage/faq.vue
@@ -1,9 +1,151 @@
 <template>
-    <div>자주 묻는 질문</div>
+    <div>
+        <h2 class="page-title">
+            <span class="ko">{{ this.$route.meta.title }}</span>
+        </h2>
+
+        <faqTab :faqTabData="faqTabData" :categoryCodeList="categoryCodeList" />
+        <faqList
+            :faqData="faqDataContent"
+            :isActive="isActive"
+            @edit="modifyRoute"
+            @faqToggle="faqToggle"
+            @delete="deleteBoard"
+        />
+        <Loading v-if="loadingData" />
+        <div class="btn-tbl-box">
+            <div class="right">
+                <router-link to="/mypage/faq/form" class="btn-form-gray">
+                    <span>등록</span>
+                </router-link>
+            </div>
+        </div>
+        <Pagination
+            v-if="faqDataContent.length"
+            :itemLength="itemLength"
+            :pageCount="pageCount"
+            :totalItem="totalItem"
+            @handleCurrentChange="handleCurrentChange"
+        />
+    </div>
 </template>
 
 <script>
-export default {};
-</script>
+import { getCustomerList, deleteCustomer } from '@/api/customer';
+import { getCode } from '@/api/code';
 
+export default {
+    name: 'faq',
+    data() {
+        return {
+            faqData: null,
+            faqDataContent: [],
+            faqTabData: null,
+            noticeArticleSeq: null,
+            page: 0,
+            pageCount: 11,
+            totalItem: 0,
+            itemLength: 10,
+            loadingData: false,
+            isActive: null,
+            categoryCodeList: {
+                listSortOptions: [],
+                value: '',
+            },
+        };
+    },
+    components: {
+        faqList: () => import('@/components/faq/list'),
+        faqTab: () => import('@/components/faq/tab'),
+        Pagination: () => import('@/components/pagination/'),
+    },
+    mounted() {
+        this.getFaqList();
+        this.getCategoryCode();
+    },
+    activated() {
+        if (this.$store.state.reload) {
+            this.getFaqList();
+            this.$store.commit('SET_RELOAD', false);
+        }
+    },
+    watch: {},
+    methods: {
+        //FAQ 리스트
+        async getFaqList() {
+            try {
+                const {
+                    data: { data: response },
+                } = await getCustomerList(this.$route.meta.sectionCode, {
+                    page: this.page,
+                    size: this.itemLength,
+                    keyword: this.searchKeyword,
+                });
+                this.faqData = response;
+                this.faqDataContent = response.content;
+                this.totalItem = this.faqData.totalElements;
+
+                //게시물 번호 //총게시물 - (현재 페이지 * 한 페이지 게시물 수) -  index = number
+                this.faqDataContent.forEach((el, index) => {
+                    el.number =
+                        this.totalItem - this.page * this.itemLength - index;
+                });
+            } catch (error) {
+                console.log(error);
+            }
+        },
+        //게시물 삭제
+        async deleteBoard(id) {
+            if (!confirm('삭제 하시겠습니까?')) {
+                return false;
+            }
+            try {
+                await deleteCustomer(id);
+                await this.getFaqList();
+            } catch (error) {
+                console.log(error);
+            }
+        },
+
+        //게시판 수정페이지 이동
+        modifyRoute(seq) {
+            this.$router.push(`/mypage/faq/modify/${seq}`);
+        },
+
+        //리스트 아코디언
+        faqToggle(seq) {
+            if (seq === this.isActive) {
+                this.isActive = null;
+            } else {
+                this.isActive = seq;
+            }
+        },
+
+        // 페이징
+        handleCurrentChange(val) {
+            this.page = val;
+            this.getFaqList();
+        },
+
+        // 카테고리 코드 목록 조회
+        async getCategoryCode() {
+            try {
+                const {
+                    data: { data: response },
+                } = await getCode('NOTICE_CATEGORY_CODE');
+                //console.log(response);
+
+                response.forEach((el) => {
+                    this.categoryCodeList.listSortOptions.push({
+                        value: el.code,
+                        label: el.codeName,
+                    });
+                });
+            } catch (error) {
+                console.log(error);
+            }
+        },
+    },
+};
+</script>
 <style scoped></style>
