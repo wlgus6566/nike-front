3f2423dd941cb0e651c326a1c78b79facceaecd0
지선애_이모션 <seonae.ji@emotion.co.kr>
2020-08-13 18:31:47 +0900

    모바일 팝업

--- frontend-m/src/main.js
+++ frontend-m/src/main.js
@@ -2,11 +2,22 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import VModal from 'vue-js-modal';
 
 Vue.config.productionTip = false;
 
 Vue.use(require('vue-moment'));
-
+Vue.use(VModal, {
+    dynamicDefaults: {
+        width: '800px',
+        height: 'auto',
+        adaptive: false,
+        draggable: true,
+        scrollable: true,
+        reset: true,
+        dynamic: true,
+    },
+});
 new Vue({
     router,
     store,
