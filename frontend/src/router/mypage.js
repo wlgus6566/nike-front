f045d00231da972b84ed6413407c6942caa05aad
jungmin.lee <jungmin.lee@emotion.co.kr>
2020-08-14 12:10:23 +0900

    router  수정

--- frontend/src/router/mypage.js
+++ frontend/src/router/mypage.js
@@ -65,7 +65,7 @@ const routes = [
     {
         path: '/mypage/notice/',
         name: 'notice',
-        component: pages('mypage/notice-list'),
+        component: pages('mypage/notice'),
         meta: {
             layout: 'Default',
             aside: 'Default',
