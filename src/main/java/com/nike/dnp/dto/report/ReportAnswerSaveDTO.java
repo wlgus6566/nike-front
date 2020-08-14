df80baaf7b244969b68661403d92461787a81a44
sojeong.lee <sojeong.lee@emotion.co.kr>
2020-08-14 18:26:50 +0900

    [보고서] 댓글 validtaion 추가

--- src/main/java/com/nike/dnp/dto/report/ReportAnswerSaveDTO.java
+++ src/main/java/com/nike/dnp/dto/report/ReportAnswerSaveDTO.java
@@ -2,8 +2,12 @@ package com.nike.dnp.dto.report;
 
 import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
+import org.hibernate.validator.constraints.Length;
 
+import javax.validation.constraints.Max;
 import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
 
 /**
  * The Class Report answer save dto.
@@ -23,7 +27,7 @@ public class ReportAnswerSaveDTO {
      * The Report seq
      * @author [이소정]
      */
-    @NotBlank(message = "reportFeedback.reportSeq")
+    @NotNull(message = "reportFeedback.reportSeq")
     @ApiModelProperty(name = "reportSeq", value = "보고서 시퀀스", example = "2", required = true)
     private Long reportSeq;
 
@@ -31,6 +35,7 @@ public class ReportAnswerSaveDTO {
      * The Answer contents
      * @author [이소정]
      */
+    @Length(max=100)
     @NotBlank(message = "reportFeedback.answerContents")
     @ApiModelProperty(name = "answerContents", value = "답글 내용", example = "Attract window graphic 1을 추가로 시공했으면 합니다.", required = true)
     private String answerContents;
