05f1550a4e5e11534cef4997bacf46d7b9ead02e
지선애_이모션 <seonae.ji@emotion.co.kr>
2020-08-18 21:50:43 +0900

    모바일 select

--- frontend-m/src/views/pages/testPage/testPage.vue
+++ frontend-m/src/views/pages/testPage/testPage.vue
@@ -1,5 +1,7 @@
 <template>
     <div>
+        <FilterSelect :selectList="selectList"></FilterSelect>
+        <CascaderSelect :cascaderList="authority"></CascaderSelect>
         <button type="button" @click="openModal">팝업</button> <br />
         <button type="button" @click="openModal2">팝업 full</button>
         <div class="modal-wrap">
@@ -11,18 +13,50 @@
 <script>
 import ModalEx from '@/components/modal-ex';
 import ModalFull from '@/components/modal-ex/full.vue';
+import FilterSelect from '@/components/filter-select';
+import CascaderSelect from '@/components/cascader-select';
+import { getAuthCacheList } from '@/api/auth';
+
 export default {
     data() {
         return {
+            selectList: {
+                value: '최신순',
+                options: [
+                    {
+                        label: '최신순',
+                        value: '최신순',
+                    },
+                    {
+                        label: '이름순',
+                        value: '이름순',
+                    },
+                ],
+            },
+            authority: {
+                value: [null],
+                options: [
+                    {
+                        value: null,
+                        label: '전체 그룹',
+                    },
+                ],
+            },
             visible: {
                 modalEx: false,
                 modalFull: false,
             },
+            userDataList: '',
         };
     },
     components: {
         ModalEx,
         ModalFull,
+        FilterSelect,
+        CascaderSelect,
+    },
+    created() {
+        this.authCacheList();
     },
     mounted() {},
     methods: {
@@ -31,6 +65,37 @@ export default {
         },
         openModal2() {
             this.visible.modalFull = true;
+        },
+        //권한 조회
+        async authCacheList() {
+            try {
+                const {
+                    data: { data: response },
+                } = await getAuthCacheList();
+                console.log(response);
+                this.recursionFn(response, this.authority.options, 1);
+            } catch (error) {
+                console.log(error);
+            }
+        },
+        recursionFn(data, item, minIndx) {
+            let _minIndx = minIndx;
+            if (minIndx === undefined) {
+                _minIndx = 0;
+            }
+            data.forEach((el, index) => {
+                item.push({
+                    value: el.authSeq,
+                    label: el.authName,
+                });
+                if (el.subAuths) {
+                    item[index + _minIndx].children = [];
+                    this.recursionFn(
+                        el.subAuths,
+                        item[index + _minIndx].children
+                    );
+                }
+            });
         },
     },
 };
