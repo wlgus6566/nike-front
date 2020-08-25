412681ed01ef19f4a6ca03db6c016aed615c18f5
jihoon.oh <jihoon.oh@emotion.co.kr>
2020-08-25 18:03:51 +0900

    [ojh] 2020.08.25 17:03  - cors 관련 테스트

--- src/main/java/com/nike/dnp/config/SecurityConfig.java
+++ src/main/java/com/nike/dnp/config/SecurityConfig.java
@@ -175,7 +175,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	 */
 	@Override
 	protected void configure(final HttpSecurity http) throws Exception {
-		http.cors().configurationSource(corsConfigurationSource())
+		http.cors()
 			.and()
 				.authorizeRequests()
 				.accessDecisionManager(accessDecisionManager())
