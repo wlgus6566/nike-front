1e7f3d637bbc08655cd70e94f024264d9dbcc4b3
지선애_이모션 <seonae.ji@emotion.co.kr>
2020-08-28 16:57:00 +0900

    모바일 뉴스 리스트, 공지사항 리스트 로딩 및 페이징 수정

--- frontend-m/src/views/pages/mypage/news-list.vue
+++ frontend-m/src/views/pages/mypage/news-list.vue
@@ -1,82 +1,113 @@
 <template>
     <div>
         <div class="sorting-area">
-            <p class="total">전체 <strong>({{totalElements}})</strong></p>
+            <p class="total">
+                전체 <strong>({{ totalElements }})</strong>
+            </p>
             <div class="search-input" :class="{ active: isActive }">
                 <div class="input-box">
-                    <input type="text" placeholder="검색어를 입력해주세요." @keyup.enter="searchInputActive" v-model="keyword"/>
-                    <button type="button" class="search" @click="searchInputActive"><span>검색</span></button>
+                    <input
+                        type="text"
+                        placeholder="검색어를 입력해주세요."
+                        @keyup.enter="searchInputActive"
+                        v-model="keyword"
+                    />
+                    <button
+                        type="button"
+                        class="search"
+                        @click="searchInputActive"
+                    >
+                        <span>검색</span>
+                    </button>
                 </div>
-                <div class="btn-txt" @click="searchInputInactive"><span>취소</span></div>
+                <div class="btn-txt" @click="searchInputInactive">
+                    <span>취소</span>
+                </div>
             </div>
         </div>
-        <div class="news-list" v-if="newsData.length > 0">
-            <div class="news-list-item" v-for="(item , index) in newsData" :key="index">
-                <a :href="`/mypage/news/detail/${item.noticeArticleSeq}`">
-                    <span class="thumbnail">
-                        <img :src="item.thumbnailFilePhysicalName" alt="">
-                    </span>
-                    <span class="info-box">
-                        <p class="title">{{item.title}}</p>
-                        <span class="date">{{item.updateDt}}</span>
-                    </span>
-                </a>
+        <template v-if="newsData">
+            <div class="news-list" v-if="newsData.length > 0">
+                <div
+                    class="news-list-item"
+                    v-for="(item, index) in newsData"
+                    :key="index"
+                >
+                    <a :href="`/mypage/news/detail/${item.noticeArticleSeq}`">
+                        <span class="thumbnail">
+                            <img :src="item.thumbnailFilePhysicalName" alt="" />
+                        </span>
+                        <span class="info-box">
+                            <p class="title">{{ item.title }}</p>
+                            <span class="date">{{ item.updateDt }}</span>
+                        </span>
+                    </a>
+                </div>
             </div>
-        </div>
-        <NoData v-else-if="newsData.length === 0 && keyword !== ''">
-            <i class="icon-search"></i>
-            <p class="desc">검색 결과가 없습니다.</p>
-        </NoData>
-        <NoData v-else>
-            <i class="icon-upload"></i>
-            <p class="desc">등록된 데이터가 없습니다.</p>
-        </NoData>
-        <Pagination
+            <template v-else>
+                <NoData v-if="keyword === ''">
+                    <i class="icon-upload"></i>
+                    <p class="desc">등록된 데이터가 없습니다.</p>
+                </NoData>
+                <NoData v-else>
+                    <i class="icon-search"></i>
+                    <p class="desc">검색 결과가 없습니다.</p>
+                </NoData>
+            </template>
+        </template>
+        <Loading v-if="loadingData" />
+        <template v-if="newsData">
+            <Pagination
                 v-if="newsData.length"
                 :itemLength="itemLength"
                 :pageCount="pageCount"
                 :totalItem="totalElements"
                 @handleCurrentChange="handleCurrentChange"
-        />
-        <Loading v-if="loadingData" />
+            />
+        </template>
     </div>
 </template>
 <script>
-import {getCustomerList} from "@/api/customer/";
+import { getCustomerList } from '@/api/customer/';
 
 export default {
     name: 'news-list',
     data() {
         return {
-            newsList: {},
-            newsData: [],
+            newsList: null,
+            newsData: null,
             page: 0,
             pageCount: 11,
             itemLength: 10,
             keyword: '',
             totalElements: 0,
             isActive: false,
-            loadingData: false
-        }
+            loadingData: false,
+        };
     },
     components: {
         Pagination: () => import('@/components/pagination/'),
         Loading: () => import('@/components/loading/'),
-        NoData: () => import('@/components/no-data')
+        NoData: () => import('@/components/no-data'),
     },
     mounted() {
-        this.getNewsList();
+        this.initListData();
     },
     methods: {
+        initListData() {
+            this.totalElements = 0;
+            this.page = 0;
+            this.newsData = null;
+            this.getNewsList();
+        },
         async getNewsList() {
             this.loadingData = true;
             try {
                 const {
                     data: { data: response },
-                } = await getCustomerList("NEWS", {
+                } = await getCustomerList('NEWS', {
                     page: this.page,
                     size: this.itemLength,
-                    keyword: this.keyword
+                    keyword: this.keyword,
                 });
                 this.newsList = response;
                 this.newsData = response.content;
@@ -86,23 +117,25 @@ export default {
                 console.log(error);
             }
         },
-        searchInputActive: function () {
+        searchInputActive: function() {
             if (this.isActive) {
-                this.getNewsList();
+                this.initListData();
             } else {
                 this.isActive = true;
             }
         },
-        searchInputInactive: function () {
+        searchInputInactive: function() {
             this.isActive = false;
-            this.keyword = "";
-            this.getNewsList();
+            this.keyword = '';
+            this.initListData();
         },
         handleCurrentChange(val) {
             this.page = val;
+            this.page = val;
+            this.newsData = null;
             this.getNewsList();
-        }
-    }
+        },
+    },
 };
 </script>
 <style scoped></style>
