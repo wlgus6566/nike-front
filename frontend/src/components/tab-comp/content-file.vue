74ab55c2d291d580d0b928a650e5f9b7bcc9a104
지선애_이모션 <seonae.ji@emotion.co.kr>
2020-09-01 10:16:20 +0900

    PC 워터마크 컨텐츠 다운로드 수정

--- frontend/src/components/tab-comp/content-file.vue
+++ frontend/src/components/tab-comp/content-file.vue
@@ -174,11 +174,18 @@ export default {
                         );
                         const link = document.createElement('a');
                         link.href = url;
+                        link.setAttribute('download', el.fileName);
+                        link.click();
+                        /* const url = window.URL.createObjectURL(
+                            new Blob([response.data])
+                        );
+                        const link = document.createElement('a');
+                        link.href = url;
                         link.seq = el.contentsBasketSeq;
                         link.setAttribute('download', el.fileName);
                         document.body.appendChild(link);
                         this.link.push(link);
-                        window.URL.revokeObjectURL(url);
+                        window.URL.revokeObjectURL(url);*/
                     } catch (error) {
                         console.error(error);
                     }
