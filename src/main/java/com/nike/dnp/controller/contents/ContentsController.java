e4d31dc1195db8691e2336f1a3cfed373c3f6678
sojeong.lee <sojeong.lee@emotion.co.kr>
2020-09-02 14:45:22 +0900

    [콘텐츠] 충돌 fix

--- src/main/java/com/nike/dnp/controller/contents/ContentsController.java
+++ src/main/java/com/nike/dnp/controller/contents/ContentsController.java
@@ -282,20 +282,10 @@ public class ContentsController {
      */
     @ApiOperation(value = "컨텐츠 파일 다운로드", notes = REQUEST_CHARACTER)
     @GetMapping(name = "컨텐츠 파일 다운로드", value = "/download/{contentsFileSeq}")
-<<<<<<< HEAD
-=======
-    @CrossOrigin(origins = {"https://www.nikespace.co.kr",
-            "http://www.nikespace.co.kr",
-            "https://devwww.nikespace.co.kr",
-            "http://devwww.nikespace.co.kr",
-            "http://localhost:8081",
-            "https://localhost:8081"},maxAge = 3600,methods = {RequestMethod.GET,RequestMethod.OPTIONS})
->>>>>>> product
     public ResponseEntity<Resource> downloadContents(
             @ApiParam(name="contentsFileSeq", value = "컨텐츠 파일 시퀀스", defaultValue = "1", required = true) @PathVariable final Long contentsFileSeq
     ) throws IOException {
         return contentsFileService.downloadFile(contentsFileSeq);
-//        return responseService.getSuccessResult();
     }
 
     /**
