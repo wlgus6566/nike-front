c9c3eb334c87d729f52e0cdb37e983c5300fb044
지선애_이모션 <seonae.ji@emotion.co.kr>
2020-09-04 11:02:06 +0900

    PC 캘린더 컬러수정
    MO
    유저정보 반영

--- frontend-m/src/components/app-aside/user-info.vue
+++ frontend-m/src/components/app-aside/user-info.vue
@@ -1,13 +1,30 @@
 <template>
     <div class="user-info">
-        <span class="store-name">홍대 SKNRS</span>
-        <p class="store">WINWIN OFFICE</p>
-        <a href="mailto:nike@win-win.co.kr" class="mail">nike@win-win.co.kr</a>
+        <span class="store-name">{{ userNickname }}</span>
+        <p class="store">{{ userAuthName }}</p>
+        <a :href="`mailto:${userIdVal}`" class="mail">{{ userIdVal }}</a>
     </div>
 </template>
 <script>
+import {
+    getAuthNameFromCookie,
+    getUserIdFromCookie,
+    getUserNickFromCookie,
+} from '@/utils/cookies';
+
 export default {
     name: 'user-info',
+    computed: {
+        userNickname() {
+            return this.$store.state.nick || getUserNickFromCookie();
+        },
+        userIdVal() {
+            return this.$store.state.user || getUserIdFromCookie();
+        },
+        userAuthName() {
+            return this.$store.state.authName || getAuthNameFromCookie();
+        },
+    },
 };
 </script>
 <style scoped></style>
