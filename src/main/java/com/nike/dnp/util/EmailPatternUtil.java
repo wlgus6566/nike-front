b1bb71861645a1e6fcf47c48e78f31f05c19e195
killbeV <jh_st@kakao.com>
2020-09-08 15:59:55 +0900

    [ojh] 2020.09.09 15:39  - email 마스킹 수정

--- src/main/java/com/nike/dnp/util/EmailPatternUtil.java
+++ src/main/java/com/nike/dnp/util/EmailPatternUtil.java
@@ -41,10 +41,26 @@ public class EmailPatternUtil {
      * @param email the email
      * @return the string
      * @author [오지훈]
-     * @implNote 앞의 2자리 제외 마스킹 처리, @뒷부분 유지
+     * @implNote 뒤 3자리 마스킹 처리, @뒷부분 유지
      * @since 2020. 9. 7. 오후 12:09:29
      */
     public String maskingEmail(final String email) {
+        String returnEmail = email;
+        if (email.length() > 3) {
+            if (email.substring(0, email.indexOf("@")).length() > 3) {
+                returnEmail = email.substring(0, email.indexOf("@") - 3) + "***" + email.substring(email.indexOf("@"));
+            }
+        }
+        return returnEmail;
+    }
+
+    /**
+     * Masking email 2 string.
+     *
+     * @param email the email
+     * @return the string
+     */
+    public String maskingEmail2(final String email) {
         String returnEmail = email;
         if (email.length() > 2) {
             StringBuilder strBuilder = new StringBuilder();
@@ -55,5 +71,4 @@ public class EmailPatternUtil {
         }
         return returnEmail;
     }
-
 }
