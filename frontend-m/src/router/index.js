9097c4f2c7a74487e9cd22fc93cbc439d18edf85
지선애_이모션 <seonae.ji@emotion.co.kr>
2020-09-14 19:34:10 +0900

    MO 레이아웃 문제 수정중

--- frontend-m/src/router/index.js
+++ frontend-m/src/router/index.js
@@ -1,11 +1,6 @@
 import Vue from 'vue';
 import VueRouter from 'vue-router';
 
-// Import methods
-import store from '@/store';
-import { pages } from '@/utils/global-methods';
-import { getAuthFromCookie } from '@/utils/cookies';
-
 // Routes
 import LoginRoutes from './login';
 import FoundationRoutes from './foundation';
@@ -16,6 +11,12 @@ import MyPageRoutes from './mypage';
 import TermsRoutes from './terms';
 import testRoutes from './test';
 import ErrorRoutes from './error';
+
+// Import methods
+import store from '@/store';
+import { pages } from '@/utils/global-methods';
+import { getAuthFromCookie } from '@/utils/cookies';
+import bus from '@/utils/bus';
 
 Vue.use(VueRouter);
 
@@ -56,20 +57,21 @@ const router = new VueRouter({
 });
 
 router.beforeEach((to, from, next) => {
+    store.state.saveFolder = false;
     if (to.meta.unauthorized) {
-        if (store.getters['isLoggedIn'] || getAuthFromCookie()) {
+        if (store.state.token || getAuthFromCookie()) {
             next('/');
         } else {
             next();
         }
     } else {
-        if (store.getters['isLoggedIn'] || getAuthFromCookie()) {
+        if (store.state.token || getAuthFromCookie()) {
+            bus.$emit('pageLoading', false);
             next();
         } else {
             next('/login');
         }
     }
-    next();
 });
 
 export default router;
