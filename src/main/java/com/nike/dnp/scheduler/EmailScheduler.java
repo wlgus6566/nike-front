291b126cd6976b9ee93bb473391ffcac758f815f
jihoon.oh <jihoon.oh@emotion.co.kr>
2020-10-05 17:14:12 +0900

    [ojh] 2020.10.05 17:14  - email test

--- src/main/java/com/nike/dnp/scheduler/EmailScheduler.java
+++ src/main/java/com/nike/dnp/scheduler/EmailScheduler.java
@@ -5,7 +5,11 @@ import com.nike.dnp.service.user.UserMailService;
 import com.nike.dnp.service.user.UserService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
 
 /**
@@ -31,12 +35,22 @@ public class EmailScheduler {
 	 * @since 2020. 9. 11. 오후 3:08:20
 	 */
 	//@Scheduled(cron = "0 0 8 * * *")
-	//@Scheduled(cron = "0 40 16 * * *")
+	@Scheduled(cron = "0 35 17 * * *")
 	public void sendUserMailByPasswordChangeOne() {
 		log.info("EmailScheduler.sendUserMailByPasswordChangeOne");
+		try {
+			final String prodIp = "10.0.1.93";
+			final String testIp = "10.0.11.39";
+			final InetAddress ip = InetAddress.getLocalHost();
+			if (prodIp.equals(ip.getHostAddress()) || testIp.equals(ip.getHostAddress())) {
+				for (User user : userService.findByPasswordChange(1)) {
+					userMailService.sendMailForChangePassword(user);
+				}
+			} else {
+				log.info("Don't Worry");
+			}
+		} catch (UnknownHostException exception) {
 
-		for (User user : userService.findByPasswordChange(1)) {
-			userMailService.sendMailForChangePassword(user);
 		}
 	}
 
