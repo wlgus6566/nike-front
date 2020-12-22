a45c79f13929d052c3603047e543952980a70d03
이소정_이모션 <sojeong.lee@emotion.co.kr>
2020-12-22 11:42:52 +0900

    [리포트] 기본파일목록사이즈 20->30

--- src/main/java/com/nike/dnp/dto/report/ReportFileSearchDTO.java
+++ src/main/java/com/nike/dnp/dto/report/ReportFileSearchDTO.java
@@ -2,6 +2,7 @@ package com.nike.dnp.dto.report;
 
 import com.nike.dnp.dto.SearchDTO;
 import io.swagger.annotations.ApiModelProperty;
+import io.swagger.annotations.ApiParam;
 import lombok.*;
 
 
@@ -16,7 +17,7 @@ import lombok.*;
 @ToString
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
 @AllArgsConstructor
-public class ReportFileSearchDTO extends SearchDTO {
+public class ReportFileSearchDTO {
 
     /**
      * 보고서 시퀀스
@@ -25,4 +26,23 @@ public class ReportFileSearchDTO extends SearchDTO {
      */
     @ApiModelProperty(name = "reportSeq", value = "보고서 시퀀스", example = "2", hidden = true)
     private Long reportSeq;
+
+
+    /**
+     * The Page
+     *
+     * @author [오지훈]
+     * @defaultValue 0
+     */
+    @ApiParam(value = "페이지", name = "page", defaultValue = "0")
+    private Integer page = 0;
+
+    /**
+     * The Size
+     *
+     * @author [오지훈]
+     * @defaultValue 20
+     */
+    @ApiParam(value = "사이즈", name = "size", defaultValue = "30")
+    private int size = 30;
 }
