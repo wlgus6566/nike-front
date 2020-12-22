18977c3a2e6c6a5b9ad1fc99f7b1d7dd1c0c1412
Seon <seonyeaji@gmail.com>
2020-12-22 17:54:28 +0900

    PC - 알람메일 팝업 퍼블리싱, 알람메일 메일폼 퍼블리싱

--- frontend/src/views/pages/common/modal-alarm.vue
+++ frontend/src/views/pages/common/modal-alarm.vue
@@ -1,37 +1,63 @@
 <template>
-  <el-dialog
-      title=""
-      class="modal-wrap"
-      width="600px"
-      :visible="visible"
-      :append-to-body="true"
-      @close="$emit('update:visible', false)"
-  >
-    <h3 class="form-title mt0">알림메일 전송</h3>
-    <el-scrollbar wrap-class="wrap" view-class="view-box" :native="false">
-      <div class="el-dialog__inner">
-        <h4>NIKE SPACE 컨텐츠 업데이트 알림</h4>
-        <p>
-          ASSET(SP21 NSW MULTI AM EXCEE)이(가) 업데이트 되었습니다.
-        </p>
-      </div>
-      <h5>comment</h5>
-      <span class="textarea">
-        <textarea></textarea>
-      </span>
-    </el-scrollbar>
+    <el-dialog
+        title=""
+        class="modal-wrap"
+        width="600px"
+        :visible="visible"
+        :append-to-body="true"
+        @close="$emit('update:visible', false)"
+    >
+        <el-scrollbar wrap-class="wrap" view-class="view-box" :native="false">
+            <div class="el-dialog__inner">
+                <h3 class="form-title mt0">알림메일 전송</h3>
+                <hr class="hr-black mt10" />
+                <h4 class="alarm-title">
+                    NIKE SPACE<br />컨텐츠 업데이트 알림
+                </h4>
+                <p class="alarm-txt">
+                    <strong>ASSET(SP21 NSW MULTI AM EXCEE)</strong><br />
+                    이(가) 업데이트 되었습니다.
+                </p>
+                <hr class="hr-gray mt40" />
+                <h3 class="sub-title mt30">COMMENT</h3>
+                <div class="mt10">
+                    <span class="textarea">
+                        <textarea style="height: 80px;" />
+                    </span>
+                </div>
+            </div>
+        </el-scrollbar>
 
-    <div slot="footer" class="dialog-footer">
-      <button type="button" class="btn-s-black">
-        <span>알림메일 전송</span>
-      </button>
-    </div>
-  </el-dialog>
+        <div slot="footer" class="dialog-footer">
+            <button type="button" class="btn-s-black">
+                <span>알림메일 전송</span>
+            </button>
+        </div>
+    </el-dialog>
 </template>
 <script>
 export default {
-  name: "modalAlarm",
-  props: ['visible'],
-}
+    name: 'modalAlarm',
+    props: ['visible'],
+};
 </script>
-<style scoped></style>
+<style scoped>
+.alarm-title {
+    display: block;
+    margin-top: 30px;
+    font-size: 30px;
+    line-height: 43px;
+    font-weight: bold;
+    color: #000;
+}
+.alarm-txt {
+    display: block;
+    margin-top: 25px;
+    font-size: 14px;
+    line-height: 24px;
+    color: #000;
+}
+.hr-gray {
+    background: #eee;
+}
+</style>
