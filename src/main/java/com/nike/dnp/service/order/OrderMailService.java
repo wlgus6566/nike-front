1025c59dabc0e5bac5cbdc403e019e9db98dd84e
이소정_이모션 <sojeong.lee@emotion.co.kr>
2021-01-08 21:02:25 +0900

    [부자재오더] - 주문서 메일 폼 적용 - 1차 주문삭제 후 2차 주문내용적용

--- src/main/java/com/nike/dnp/service/order/OrderMailService.java
+++ src/main/java/com/nike/dnp/service/order/OrderMailService.java
@@ -1,29 +1,13 @@
 package com.nike.dnp.service.order;
 
-import com.nike.dnp.common.mail.MailService;
-import com.nike.dnp.common.variable.ServiceCode;
-import com.nike.dnp.dto.email.OrderProductDTO;
-import com.nike.dnp.dto.email.SendDTO;
-import com.nike.dnp.dto.order.OrderProductMappingSaveDTO;
-import com.nike.dnp.dto.order.OrderProductResultDTO;
-import com.nike.dnp.dto.order.OrderSaveDTO_del;
 import com.nike.dnp.dto.user.UserDTO;
-import com.nike.dnp.entity.order.OrderEntity;
 import com.nike.dnp.entity.order.OrderMail;
-import com.nike.dnp.entity.order.OrderProductMapping;
 import com.nike.dnp.repository.order.OrderMailRepository;
-import com.nike.dnp.repository.order.OrderProductMapperRepository;
-import com.nike.dnp.util.CloudFrontUtil;
-import com.nike.dnp.util.SecurityUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.commons.lang.StringUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.util.ObjectUtils;
 
-import java.text.DecimalFormat;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
 
