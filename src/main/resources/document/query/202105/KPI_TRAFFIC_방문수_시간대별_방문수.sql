69d664ef6b1273127596f3bba267d27fb0d3ceb3
이소정_이모션 <sojeong.lee@emotion.co.kr>
2021-05-20 16:50:34 +0900

    [KPI] 5월 쿼리

---
+++ src/main/resources/document/query/202105/KPI_TRAFFIC_방문수_시간대별_방문수.sql
@@ -0,0 +1,38 @@
+-- 접속 시간 대 별 방문수 
+SELECT RESULT.TIME, COUNT(RESULT.TIME) AS COUNT
+FROM (
+         SELECT LOGIN_DT
+              , CASE
+                    WHEN LOGIN_DT BETWEEN CONCAT(DATE_FORMAT(LOGIN_DT, '%Y-%m-%d'),
+                                                 ' 00:00:00') AND CONCAT(
+                            DATE_FORMAT(LOGIN_DT, '%Y-%m-%d'), ' 02:59:59') THEN '00-03'
+                    WHEN LOGIN_DT BETWEEN CONCAT(DATE_FORMAT(LOGIN_DT, '%Y-%m-%d'),
+                                                 ' 03:00:00') AND CONCAT(
+                            DATE_FORMAT(LOGIN_DT, '%Y-%m-%d'), ' 05:59:59') THEN '03-06'
+                    WHEN LOGIN_DT BETWEEN CONCAT(DATE_FORMAT(LOGIN_DT, '%Y-%m-%d'),
+                                                 ' 06:00:00') AND CONCAT(
+                            DATE_FORMAT(LOGIN_DT, '%Y-%m-%d'), ' 08:59:59') THEN '06-09'
+                    WHEN LOGIN_DT BETWEEN CONCAT(DATE_FORMAT(LOGIN_DT, '%Y-%m-%d'),
+                                                 ' 09:00:00') AND CONCAT(
+                            DATE_FORMAT(LOGIN_DT, '%Y-%m-%d'), ' 11:59:59') THEN '09-12'
+                    WHEN LOGIN_DT BETWEEN CONCAT(DATE_FORMAT(LOGIN_DT, '%Y-%m-%d'),
+                                                 ' 12:00:00') AND CONCAT(
+                            DATE_FORMAT(LOGIN_DT, '%Y-%m-%d'), ' 14:59:59') THEN '12-15'
+                    WHEN LOGIN_DT BETWEEN CONCAT(DATE_FORMAT(LOGIN_DT, '%Y-%m-%d'),
+                                                 ' 15:00:00') AND CONCAT(
+                            DATE_FORMAT(LOGIN_DT, '%Y-%m-%d'), ' 17:59:59') THEN '15-18'
+                    WHEN LOGIN_DT BETWEEN CONCAT(DATE_FORMAT(LOGIN_DT, '%Y-%m-%d'),
+                                                 ' 18:00:00') AND CONCAT(
+                            DATE_FORMAT(LOGIN_DT, '%Y-%m-%d'), ' 20:59:59') THEN '18-21'
+                    WHEN LOGIN_DT BETWEEN CONCAT(DATE_FORMAT(LOGIN_DT, '%Y-%m-%d'),
+                                                 ' 21:00:00') AND CONCAT(
+                            DATE_FORMAT(LOGIN_DT, '%Y-%m-%d'), ' 23:59:59') THEN '21-24'
+                    ELSE 'ETC'
+             END AS TIME
+         FROM TB_USER_LOGIN_LOG TULL
+         WHERE TULL.LOGIN_DT BETWEEN '2021-04-16 00:00:00' AND '2021-05-15 23:59:59'
+           -- ZABBIX 로그인에 사용한 계정 제외
+           AND TULL.USER_SEQ != 2
+     ) RESULT
+GROUP BY RESULT.TIME
+;
---
+++ src/main/resources/document/query/202105/KPI_TRAFFIC_순방문수_일별_평균방문수.sql
@@ -0,0 +1,20 @@
+-- 일별 평균 방문수
+SELECT T.DATE
+     , SUM(T.COUNT) AS SUM
+     , COUNT(T.DATE) AS COUNT
+     , ROUND(AVG(T.COUNT), 0) AS AVERAGE
+FROM (
+         SELECT DATE_FORMAT(USER.DATE, '2021') AS DATE
+              , COUNT(USER.DATE) AS COUNT
+         FROM (
+                SELECT DATE_FORMAT(TULL.LOGIN_DT, "%Y-%m-%d") AS DATE
+                FROM TB_USER_LOGIN_LOG TULL
+                WHERE TULL.LOGIN_DT BETWEEN '2021-04-16 00:00:00' AND '2021-05-15 23:59:59'
+                -- ZABBIX 로그인에 사용한 계정 제외
+                AND TULL.USER_SEQ != 2
+                GROUP BY DATE_FORMAT(TULL.LOGIN_DT, "%Y-%m-%d"), TULL.USER_SEQ
+              ) USER
+         GROUP BY USER.DATE
+     ) T
+GROUP BY T.DATE
+;
