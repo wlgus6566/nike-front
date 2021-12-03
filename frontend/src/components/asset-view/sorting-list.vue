72b1ba06b0cf233ed7a088fc938413ef909fa901
choimeeyoung <choimy8018@naver.com>
2021-12-03 15:00:13 +0900

    2021.12.03 MyPage 최근본폴더/내가 업로드한 폴더 카운트 수 삭제

--- frontend/src/components/asset-view/sorting-list.vue
+++ frontend/src/components/asset-view/sorting-list.vue
@@ -11,7 +11,7 @@
             >
                 <span>
                     {{ item.title }}
-
+                  <!--                    <em v-if="item.count !== '00'">({{ item.count }})</em>-->
                 </span>
             </button>
         </li>
