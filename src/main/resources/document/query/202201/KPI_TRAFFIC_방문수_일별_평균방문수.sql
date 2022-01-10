1920fcc97e3c2b061f4da97cdf20ee935369292a
choimeeyoung <choimy8018@naver.com>
2022-01-10 12:32:47 +0900

    2022.01.10 KPI Query 수정 [KPI_DEPTH_그룹_계정_수.sql / : KPI_DEPTH_하위_그룹_계정_수.sql]

---
+++ src/main/resources/document/query/202201/KPI_TRAFFIC_방문수_일별_평균방문수.sql
@@ -0,0 +1,15 @@
+-- 일별 평균 방문수
+    SELECT USER.DATE
+         , SUM(USER.COUNT) AS SUM
+         , COUNT(USER.DATE) AS COUNT
+         , ROUND(AVG(USER.COUNT), 0) AS AVERAGE
+    FROM (
+             SELECT DATE_FORMAT(TULL.LOGIN_DT, '2021') AS DATE, COUNT(*) AS COUNT
+             FROM TB_USER_LOGIN_LOG TULL
+             WHERE LOGIN_DT BETWEEN '2021-12-16 00:00:00' AND '2022-01-15 23:59:59'
+               -- ZABBIX 로그인에 사용한 계정 제외
+             AND TULL.USER_SEQ != 2
+             GROUP BY DATE_FORMAT(TULL.LOGIN_DT, '%Y-%m-%d')
+         ) USER
+    GROUP BY USER.DATE
+;
